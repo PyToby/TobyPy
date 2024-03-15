74bb1da0ce04db25a58583946bcfe07889b52638
PyToby <toby.radkovsky@gmail.com>
2024-03-15 14:17:24 +0100

    update na kostku

--- python/kostka.py
+++ python/kostka.py
@@ -5,12 +5,39 @@ import random as ran
 class Dice:
 
     def __init__(self, number_of_sides=6):
-        self.number_of_sides = number_of_sides
+        self.__number_of_sides = number_of_sides
 
     def roll(self):
-        return ran.randint(1, self.number_of_sides)
+        return ran.randint(1, self.__number_of_sides)
+
+    def __str__(self):
+        return f"Toto je kostka s {self.__number_of_sides} stenami"
+
+    def getNum_of_sides(self):
+        return self.__number_of_sides
+
+    def setNum_of_sides(self, number_of_sides):
+        if number_of_sides > 0:
+            self.__number_of_sides = number_of_sides
+        else:
+            print("Cislo musi byt kladne")
+
+    def setNum_of_sides_man(self):
+        try:
+            number_of_sides = int(input("Insert a number:"))
+            if number_of_sides > 0:
+                self.__number_of_sides = number_of_sides
+            else:
+                print("Zadej cislo vetsi nez 0")
+        except:
+            print("Zadani musi byt kladne cislo")
+
 
 if __name__ == "__main__":
-    number_of_sides = int(input("Insert a number:"))
-    d = Dice(number_of_sides)
+    d = Dice()
+
+    d.setNum_of_sides(20)
+    d.setNum_of_sides_man()
     print(d.roll())
+    print(d)
+    
