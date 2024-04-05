c48b9d4de42fda8f9b28e924b7df8064826b0328
PyToby <toby.radkovsky@gmail.com>
2024-04-05 14:17:21 +0200

    update a souper

--- python/bojovnik.py
+++ python/bojovnik.py
@@ -17,6 +17,7 @@ class Bojovnik():
         self._utok = utok
         self._obrana = obrana
         self._kostka = kostka
+        self._zprava = ''
 
     def __str__(self):
         return str(self._jmeno)
@@ -32,5 +33,28 @@ class Bojovnik():
         pocet = int(self._zivot / self._max_zivot * celkem)
         if pocet == 0 and self.je_nazivu():
             pocet = 1
-        return f"[{'#'* pocet}{' ' * (celkem - pocet)}]"
+        return f"[{'#' * pocet}{' ' * (celkem - pocet)}]"
 
+    def bran_se(self, uder):
+        zraneni = uder - (self._obrana + self._kostka.roll())
+        if zraneni > 0:
+            zprava = f'{self._jmeno} utrpel zraneni o sile {zraneni}hp.'
+            self._zivot = self._zivot - zraneni
+            if self._zivot < 0:
+                self._zivot = 0
+                zprava = f'{zprava[:-1]} a zemrel.'
+        else:
+            zprava = f'{self._jmeno} odrazil utok.'
+        self.nastav_zpravu(zprava)
+
+    def utoc(self, souper):
+        uder = self._utok + self._kostka.roll()
+        zprava = f'{self._jmeno} utoci s uderem za {uder}hp.'
+        self.nastav_zpravu(zprava)
+        souper.bran_se(uder)
+
+    def nastav_zpravu(self, zprava):
+        self._zprava = zprava
+
+    def vypis_zpravu(self):
+        return self._zprava
--- python/main_bojovnik.py
+++ python/main_bojovnik.py
@@ -5,7 +5,9 @@ from kostka import Dice
 
 kostka = Dice(10)
 bojovnik = Bojovnik("Eragon", 100, 20, 10, kostka)
-
-print(f"Bojovnik: {bojovnik}")
-print(f"Nazivu: {bojovnik.je_nazivu()}")
 print(f"Zivot: {bojovnik.grafic_zivot()}")
+souper = Bojovnik('Shadow', 80, 1800, 15, kostka)
+souper.utoc(bojovnik)
+print(souper.vypis_zpravu())
+print(bojovnik.vypis_zpravu())
+print(f'Zivot: {bojovnik.grafic_zivot()}')
