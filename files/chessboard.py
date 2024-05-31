d3b04839d6b3bf7b52e1d44ec7a61ce8d9cbf850
PyToby <toby.radkovsky@gmail.com>
2024-05-31 14:13:36 +0200

    update v chessboard

--- files/chessboard.py
+++ files/chessboard.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 
-def num_fields():
-    for num in range(1, 65):
-        if num == 64:
-            print(f'{num}.')
+def num_row():
+    for field in range(1, 65):
+        if field == 64:
+            print(f'{field}.')
         else:
-            print(f'{num}, ', end='')
+            print(f'{field}, ', end='')
+
+def num_field():
+    fields = range(1, 65)
+    for row in range(1, 9):
+        for columns in range(1, 9):
+            print(fields[columns-1], end='')
+        print()
 
 if __name__ == '__main__':
-    num_fields()
+    num_field()
